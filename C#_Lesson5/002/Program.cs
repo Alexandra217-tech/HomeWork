a3c722a046c961bb1ecb23ebfda4a7cccdb7f30b
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-01 20:04:07 +0300

    Вторая задача к пятому семинару

---
+++ C#_Lesson5/002/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] array = new int[123];
+int CountElemets = 0;
+void FillArray (int [] collection)
+{
+    int len = collection.Length;
+    for (int i=0; i<len; i++)
+    {
+        collection[i] = new Random().Next(0, 150);
+    }
+    for (int h=0; h<len; h++)
+    {
+        if (collection[h]>9) 
+        {
+            if (collection[h]<100)  
+            {
+                CountElemets = CountElemets + 1;
+            }
+        }
+    }
+}
+
+void PrintArray (int [] coll)
+{
+    int count =coll.Length;
+    for (int j=0; j<count; j++)
+    {
+        Console.WriteLine (coll[j]);
+    }
+}
+
+FillArray(array);
+//PrintArray(array);
+Console.WriteLine (CountElemets);
