bc2c1a781b9235f8cba6f67305628c2a39ba08d9
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-01 20:12:32 +0300

    Третья задача к пятому семинару

---
+++ C#_Lesson5/003/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[10];
+int sum = 0;
+int len = array.Length;
+for (int i=0; i<len; i++)
+{
+    array[i] = new Random().Next(0, 150);
+}
+for (int h=0; h<len; h++)
+{
+    if (h%2!=0) 
+    {
+       sum = sum + array [h];
+    }
+}
+
+
+void PrintArray (int [] coll)
+{
+    int count =coll.Length;
+    for (int j=0; j<count; j++)
+    {
+        Console.WriteLine (coll[j]);
+    }
+}
+
+//PrintArray(array);
+Console.WriteLine (sum);
