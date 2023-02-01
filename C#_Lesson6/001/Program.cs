ed7632f736dcab94d842ec1fae2b9361da019c84
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-01 21:01:23 +0300

    Первая задача к шестому семинару

---
+++ C#_Lesson6/001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+int[] array = new int[10];
+int[] array1 = array;
+int A = 0;
+int len = array.Length;
+for (int i=0; i<len; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+for (int h=0; h<len-1; h++)
+{
+    A = array[h];
+    array[h]=array[h+1];
+    array[h+1]=A;
+}
+
+
+
+void PrintArray (int [] coll)
+{
+    int count =coll.Length;
+    for (int j=0; j<count; j++)
+    {
+        Console.WriteLine (coll[j]);
+    }
+}
+
+PrintArray(array);
+Console.WriteLine ();
+PrintArray(array1);
