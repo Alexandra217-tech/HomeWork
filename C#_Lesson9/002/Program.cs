e5f12bc1466e1608766d6e38c6bbe3bc7da79a47
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-04 19:01:10 +0300

    Вторая задача к девятому семинару

---
+++ C#_Lesson9/002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+while (M<N+1)
+{
+    Console.Write (M);
+    M++;
+}
