2fa8b69931ec73f5c01bca538aef99eecdc12cbc
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-04 16:32:41 +0300

    Первая задача к девятому семинару

---
+++ C#_Lesson9/001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a<N+1)
+{
+    Console.Write (a);
+    a++;
+}
