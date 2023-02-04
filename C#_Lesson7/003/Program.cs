f27c98a21f7dcf5649cd3e45287a8cf7f8b99881
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-04 16:19:26 +0300

    Третья задача к седьмому семинару

---
+++ C#_Lesson7/003/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int [,] array2 = new int[5, 6];
+double sum = 0;
+int count =0;
+double SrArifm =0;
+for (int i=0; i<array2.GetLength(0); i++)
+{
+    for (int j=0; j<array2.GetLength(1); j++)
+    {
+        array2[i, j] = new Random().Next(0, 10);
+        Console.Write(array2[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.Write("----------------------------");
+    
+for (int i=0; i<6; i++)
+{
+    for (int j=0; j<5; j++)
+    {
+        sum += array2[j, i];
+        count++;
+    }
+    Console.WriteLine("  ");
+    Console.WriteLine(SrArifm=sum/count);
+}
