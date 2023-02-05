0001ea9d4e145a8c67f88360e431940a97b2f83a
Alexandra <a.a.lisenkova127@gmail.com>
2023-02-05 12:35:05 +0300

    Вторая задача из ДР к девятому семинару

--- C#_Lesson9/002/Program.cs
+++ C#_Lesson9/002/Program.cs
@@ -1,15 +1,17 @@
-﻿// Задайте значения M и N. Напишите программу, 
-//которая выведет все натуральные числа в промежутке от M до N.
+﻿// Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
 
 Console.Write("Введите M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите N: ");
 int N = Convert.ToInt32(Console.ReadLine());
+int sum =0;
 while (M<N+1)
 {
-    Console.Write (M);
+    sum+=M;
     M++;
 }
+Console.Write(sum);
 
 
 
